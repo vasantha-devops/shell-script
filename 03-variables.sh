9333d6ee3d750c8d250083dc7e53b80752333966
vasantha <devopsvas@gmail.com>
2024-09-26 12:36:29 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#declaring of variable
+PERSON1=Ramesh #no space between = and value
+PERSON2=Suresh
+
+#reffering of variable
+
+echo "$person1:: Hello $person2, How are you?"
+echo "$person2:: Hi $person1, I am fine, How are you?"
+echo "$person1: I am fine too. How is your work?"
+echo "$person2: not bad. I am thinking to upgrade to Devops."
